5e43c28e8cc93c6262b7eca6aad05ac6b0ea7032
David Wilhelm <dave@jumbledpile.com>
2018-11-17 20:41:51 -0800

    Add SciLexer.dll to the compile script

--- BuildTools/compile.bat
+++ BuildTools/compile.bat
@@ -3,7 +3,13 @@
 echo Setting up dev environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-echo Compiling
+echo Compiling notepad++.exe
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MSBuild.exe" ^
     .\PowerEditor\visual.net\notepadPlus.vcxproj ^
     /t:rebuild /p:Configuration="Unicode Release" /v:normal /maxcpucount
+
+echo Compiling SciLexer.dll
+pushd .\scintilla\win32
+nmake NOBOOST=1 -f scintilla.mak
+popd
+copy .\scintilla\bin\SciLexer.dll .\PowerEditor\bin64\
