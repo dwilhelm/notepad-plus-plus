ce764c9101ac409a88cb4f11f5cb6dd3daeb0389
David Wilhelm <dave@jumbledpile.com>
2018-11-18 12:28:58 -0800

    Ensure the compile script runs from the repo root
    On some computers the vcvars batch files change the current directory
    to ~/source. I'm not sure what the difference in setup is.
    
    Make sure to cd to the repo root path after setting up the dev env.

--- BuildTools/compile.bat
+++ BuildTools/compile.bat
@@ -1,7 +1,13 @@
 @echo off
 
-echo Setting up dev environment
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+set SRCROOT=%~d0%~p0..
+echo Set repo root path to %SRCROOT%
+
+if not defined DevEnvDir (
+    echo Setting up dev environment
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+cd %SRCROOT%
 
 echo Compiling notepad++.exe
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MSBuild.exe" ^
